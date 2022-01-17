7005157ffb24f88f356f44d69d2c899dae663a1a
mayak17 <maxim_yakovlev2012@mail.ru>
2022-01-17 15:51:51 +0300

    Initial commit

---
+++ src/main/java/com/test/javatestsv/JavaTestSvApplication.java
@@ -0,0 +1,13 @@
+package com.test.javatestsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestSvApplication.class, args);
+    }
+
+}
